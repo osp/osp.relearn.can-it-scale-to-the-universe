74bb998518fde29019595967b69419647fe9b6f8
christop <christoph@lafkon.net>
2013-08-27 19:39:30 +0200

    i/o width now included

--- inflate-space/inflate-img.sh
+++ inflate-space/inflate-img.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 
- IIMG=circle.xpm
- OIMG=test.xpm
+  IIMG=circle.gif
+  OIMG=test.xpm
 
- IMGWIDTH=100
+  OWIDTH=100
 
+  IWIDTH=`identify $IIMG | \
+          cut -d " " -f 3 | \
+          cut -d "x" -f 1`
 
-  cp $IIMG $OIMG
+  convert -resize $OWIDTH -monochrome $IIMG $OIMG
+
 
 # REPLACE SPACES WITH PLACEHOLDER
-  sed -i "/^.\{$IMGWIDTH\}/s/ /X/g" $OIMG
-
+  sed -i "/^.\{$IWIDTH\}/s/ /X/g" $OIMG
  
 # ADD A SPACE AFTER EACH CHARACTER
 # (=REPLACE ALL CHARACTERS WITH ITSELF AND A SPACE)
-  sed -i "/^.\{$IMGWIDTH\}/s/./& /g" $OIMG
-
+  sed -i "/^.\{$IWIDTH\}/s/./& /g" $OIMG
 
 # REPLACE PLACEHOLDER WITH SPACES AGAIN
-  sed -i "/^.\{$IMGWIDTH\}/s/X/ /g" $OIMG
+  sed -i "/^.\{$IWIDTH\}/s/X/ /g" $OIMG
 
+  INFLATEDWIDTH=`expr $IWIDTH \* 2`
 
+# REPLACE OLD IMG WIDTH WITH NEW (FIRST INSTANCE!)
+  sed -i "s/$IWIDTH/$INFLATEDWIDTH/" $OIMG
 
 
 
