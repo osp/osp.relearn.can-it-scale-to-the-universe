71d73bc9be78ba45bdaf4b8b5398dab8c39d2824
christop <christoph@lafkon.net>
2013-08-27 19:47:17 +0200

    adjusted script

--- inflate-space/inflate-img.sh
+++ inflate-space/inflate-img.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 
-  IIMG=circle.gif
+  IIMG=circle.png
   OIMG=test.xpm
 
   OWIDTH=100
 
-  IWIDTH=`identify $IIMG | \
-          cut -d " " -f 3 | \
-          cut -d "x" -f 1`
+#  IWIDTH=`identify $IIMG | \
+#          cut -d " " -f 3 | \
+#          cut -d "x" -f 1`
+
 
   convert -resize $OWIDTH -monochrome $IIMG $OIMG
 
-
 # REPLACE SPACES WITH PLACEHOLDER
-  sed -i "/^.\{$IWIDTH\}/s/ /X/g" $OIMG
+  sed -i "/^.\{$OWIDTH\}/s/ /X/g" $OIMG
  
 # ADD A SPACE AFTER EACH CHARACTER
 # (=REPLACE ALL CHARACTERS WITH ITSELF AND A SPACE)
-  sed -i "/^.\{$IWIDTH\}/s/./& /g" $OIMG
+  sed -i "/^.\{$OWIDTH\}/s/./& /g" $OIMG
 
 # REPLACE PLACEHOLDER WITH SPACES AGAIN
-  sed -i "/^.\{$IWIDTH\}/s/X/ /g" $OIMG
+  sed -i "/^.\{$OWIDTH\}/s/X/ /g" $OIMG
 
-  INFLATEDWIDTH=`expr $IWIDTH \* 2`
+  INFLATEDWIDTH=`expr $OWIDTH \* 2`
 
 # REPLACE OLD IMG WIDTH WITH NEW (FIRST INSTANCE!)
-  sed -i "s/$IWIDTH/$INFLATEDWIDTH/" $OIMG
+  sed -i "s/$OWIDTH/$INFLATEDWIDTH/" $OIMG
 
 
 
