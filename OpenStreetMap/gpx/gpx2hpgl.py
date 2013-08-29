c4c3300db96d147e7f27fd98b4369b17e6bee61b
Alexandre Leray <alexandre@alexandreleray.com>
2013-08-29 10:24:31 +0200

    lost in translation; I'm trying to lattitude, longitude, user coordinates and physical coordinates of the plotter

--- OpenStreetMap/gpx/gpx2hpgl.py
+++ OpenStreetMap/gpx/gpx2hpgl.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python2
 
 """
+Usage:
+    gpx2hpgl.py <file.hpgl>
 VS
 de 1 a 42 cms par secondes
 """
@@ -9,89 +11,57 @@ de 1 a 42 cms par secondes
 from lxml import etree
 import datetime
 import math
+import sys
 
 
 NS = 'http://www.topografix.com/GPX/1/0'
-FACTOR = 1000000
 
 
-hpgl = "IN\n"
+def walk(tree):
+    trkpts = tree.xpath('//gpx:trkpt', namespaces={'gpx': NS})
 
-f = open('pierreh-2013-08-28T153250.gpx')
-tree = etree.parse(f)
+    for trkpt in trkpts:
+        lon = float(trkpt.attrib['lon'])
+        lat = float(trkpt.attrib['lat'])
+        ele = trkpt.find('{%s}ele' % NS).text
+        time = trkpt.find('{%s}time' % NS).text
+        time = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
 
-trkpts = tree.xpath('//gpx:trkpt', namespaces={'gpx': NS})
+        yield dict(lon=lon, lat=lat, ele=ele, time=time)
 
+def main(f):
+    f = open(f)
+    tree = etree.parse(f)
 
-min_lon = None
-min_lat = None
-max_lon = None
-max_lat = None
+    trkpts = list(walk(tree))
 
+    min_lon = min(d['lon'] for d in trkpts)
+    max_lon = max(d['lon'] for d in trkpts)
+    min_lat = min(d['lat'] for d in trkpts)
+    max_lat = max(d['lat'] for d in trkpts)
 
+    hpgl = "IN\n"
+    hpgl += "IP0,16158,0,11040;\n"
+    hpgl += "SC%s,%s,%s,%s;\n" % (min_lon, max_lon, min_lat, max_lat)
+    hpgl += "SP1;\n"
+    hpgl += "PD;\n"
 
-for trkpt in trkpts:
-    lon = float(trkpt.attrib['lon'])
-    lat = float(trkpt.attrib['lat'])
+    for i, trkpt in enumerate(trkpts[1:]):
+        #ab = trkpt['lon'] - trkpts[i]['lon']
+        #bc = trkpt['lat'] - trkpts[i]['lat']
+        #ac = math.sqrt(ab**2 + bc**2)
+        #delta = trkpt['time'] - trkpts[i]['time']
+        #hpgl += "VS%s;\n" % delta.seconds
 
-    if lon < min_lon or min_lon is None:
-        min_lon = lon
+        lon = (((trkpt['lon'] - min_lon) / (max_lon - min_lon)) * 1000)
+        lat = (((trkpt['lat'] - min_lat) / (max_lon - min_lon)) * 1000)
 
-    if lon > max_lon or max_lon is None:
-        max_lon = lon
+        hpgl += "PA%s,%s;\n" % (lon, lat)
 
-    if lat < min_lat or min_lat is None:
-        min_lat = lat
+    hpgl += 'SP0;\n'
 
-    if lat > max_lat or max_lat is None:
-        max_lat = lat
+    print(hpgl)
 
-min_lon = min_lon * FACTOR
-min_lat = min_lat * FACTOR
-max_lon = max_lon * FACTOR
-max_lat = max_lat * FACTOR
-
-
-#print(min_lon, max_lon, min_lat, max_lat)
-
-
-
-trkpt = trkpts[0]
-
-old_lon = float(trkpt.attrib['lon']) * FACTOR
-old_lat = float(trkpt.attrib['lat']) * FACTOR
-
-old_ele = trkpt.find('{%s}ele' % NS).text
-old_time = trkpt.find('{%s}time' % NS).text
-old_time = datetime.datetime.strptime(old_time, "%Y-%m-%dT%H:%M:%SZ")
-
-hpgl += "IP0,16158,0,11040;\n"
-hpgl += "SC%s,%s,%s,%s;\n" % (min_lon, max_lon, min_lat, max_lat)
-hpgl += "SP1;\n"
-hpgl += "PD;\n"
-
-
-for trkpt in trkpts[1:]:
-    lon = float(trkpt.attrib['lon']) * FACTOR
-    lat = float(trkpt.attrib['lat']) * FACTOR
-    ele = trkpt.find('{%s}ele' % NS).text
-    time = trkpt.find('{%s}time' % NS).text
-    time = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
-
-    ab = lon - old_lon
-    bc = lat - old_lat
-
-    ac = math.sqrt(ab**2 + bc**2)
-
-    time_delta = time - old_time
-    hpgl += "VS%s;\n" % time_delta.seconds
-    hpgl += "PA%s,%s;\n" % (lon,lat)
-
-    old_lat = lat
-    old_lon = lon
-    old_ele = ele
-    old_time = time
-
-hpgl += 'SP0;\n'
-
-print(hpgl)
+if __name__ == "__main__":
+    f = sys.argv[1]
+    main(f)
